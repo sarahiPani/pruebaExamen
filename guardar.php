df83256c40f6980fc0b2da43ce020f3f1479b7d6
Sarahi Pani <sarizpl122@gmail.com>
2021-11-03 23:29:21 -0600

    prueba para que jale

---
+++ guardar.php
@@ -0,0 +1,42 @@
+<?php
+	
+	require 'conexion.php';
+	
+	$id = $_POST['id'];
+	$nombre = $_POST['nombre'];
+	$descripcion = $_POST['descripcion'];
+	$precio = $_POST['precio'];
+	
+	
+	$sql = "INSERT INTO productos (id, nombre, descripcion, precio) VALUES ('$id','$nombre', '$descripcion', '$precio')";
+	$resultado = $mysqli->query($sql);
+	
+?>
+
+<html lang="es">
+	<head>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap-theme.css" rel="stylesheet">
+		<script src="js/jquery-3.1.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>	
+	</head>
+	
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="row" style="text-align:center">
+					<?php if($resultado) { ?>
+						<h3>REGISTRO GUARDADO</h3>
+						<?php } else { ?>
+						<h3>ERROR AL GUARDAR</h3>
+					<?php } ?>
+					
+					<a href="index.php" class="btn btn-primary">Regresar</a>
+					
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
